c256f4390f3f7799d975883a4a4e753e2f895729
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-03-25 20:33:52 -0700

    Learning the Math object for randomness

--- assets/js/game.js
+++ assets/js/game.js
@@ -31,14 +31,15 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0,playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth-damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -57,7 +58,8 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+      playerHealth = Math.max(0, playerHealth-damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -72,6 +74,13 @@ var fight = function(enemyName) {
       }
     }
   };
+
+  //function to generate a random numeric value
+  var randomNumber = function() {
+    var value = Math.floor(Math.random() * 21) + 40;
+
+    return value;
+  }
 
 
 //Function to start a new game 
@@ -91,7 +100,7 @@ var fight = function(enemyName) {
                 var pickedEnemyName = enemyNames[i];
             
                 // reset enemyHealth before starting new fight
-                enemyHealth = 50;
+                enemyHealth = randomNumber(40,60);
             
                 // use debugger to pause script from running and check what's going on at that moment in the code
                 // debugger;
@@ -186,10 +195,9 @@ var fight = function(enemyName) {
       }
     }
   
-
-
     // start the game when the page loads
-    startGame();
+    startGame();                                
+
     
 
 
